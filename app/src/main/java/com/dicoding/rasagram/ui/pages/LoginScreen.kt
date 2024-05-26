df5068704443057a8d610c1309e403fe8be73087
Wery <44234831+Wery-wer@users.noreply.github.com>
2024-05-26 14:43:51 +0700

    update

--- app/src/main/java/com/dicoding/rasagram/ui/pages/LoginScreen.kt
+++ app/src/main/java/com/dicoding/rasagram/ui/pages/LoginScreen.kt
@@ -1,5 +1,6 @@
 package com.dicoding.rasagram.ui.pages
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -8,61 +9,83 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.dicoding.rasagram.R
 import com.dicoding.rasagram.ui.theme.Orange
+import com.dicoding.rasagram.ui.theme.arialFamily
+import com.dicoding.rasagram.ui.theme.poppinsFamily
 
 @Composable
 fun LoginScreen(){
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Top,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Spacer(modifier = Modifier.height(83.dp))
-        Box(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(16.dp),
-            contentAlignment = Alignment.Center
-        ) {
-            Text(
-                text = "Welcome to RasaGram!",
-                style = TextStyle(
-                    fontSize = 24.sp,
-                    fontWeight = FontWeight.Bold,
-                    color = Orange
-                )
-            )
-        }
-        Spacer(modifier = Modifier.height(79.dp))
-        Box(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(16.dp),
-            contentAlignment = Alignment.CenterStart
-        ) {
-            Text(
-                text = "Please login to continue.",
-                style = TextStyle(
-                    fontSize = 24.sp,
-                    fontWeight = FontWeight.Bold,
-                    color = Orange
-                )
-            )
-        }
-        Spacer(modifier = Modifier.height(16.dp))
-        // Add login components here
-    }
 
+    Surface(
+        modifier = Modifier.fillMaxSize().background(Color.White).padding(16.dp)
+    ){
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Spacer(modifier = Modifier.height(83.dp))
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    text = stringResource(R.string.rasagram),
+                    style = TextStyle(
+                        fontSize = 24.sp,
+                        fontWeight = FontWeight.Bold,
+                        color = Orange,
+                        fontFamily = poppinsFamily
+                    )
+                )
+            }
+            Spacer(modifier = Modifier.height(79.dp))
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(25.dp),
+                contentAlignment = Alignment.CenterStart
+            ) {
+                Column {
+                    Text(
+                        text = stringResource(R.string.sign_in),
+                        style = TextStyle(
+                            fontSize = 24.sp,
+                            fontWeight = FontWeight.Bold,
+                            color = Orange,
+                            fontFamily = arialFamily
+                        )
+                    )
+                    Text(
+                        text = stringResource(R.string.sign_in_to_continue),
+                        style = TextStyle(
+                            fontSize = 12.sp,
+                            fontWeight = FontWeight.Normal,
+                            color = Orange,
+                            fontFamily = arialFamily
+                        )
+                    )
+                }
+            }
+        }
+    }
 }
+
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun LoginScreenPreview(){
