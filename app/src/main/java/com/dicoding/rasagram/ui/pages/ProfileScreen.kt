50ef4c0ce664dbf0c5abda76166c5641fcfbe8e8
Bodan07 <najmi.kamil@gmail.com>
2024-05-29 14:37:26 +0700

    profile

--- app/src/main/java/com/dicoding/rasagram/ui/pages/ProfileScreen.kt
+++ app/src/main/java/com/dicoding/rasagram/ui/pages/ProfileScreen.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
@@ -28,6 +29,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -62,8 +64,16 @@ fun ProfileScreen() {
                         Brush.verticalGradient(
                             colors = listOf(Color.White, Orange)
                         )
-                    ) // Background color to make it visible
-            )
+                    ),
+                contentAlignment = Alignment.Center// Background color to make it visible
+            ){
+                Box(
+                    modifier = Modifier
+                        .size(150.dp)
+                        .clip(CircleShape)
+                        .background(color = Color.White)
+                )
+            }
             Spacer(modifier = Modifier.height(25.dp))
             Text(
                     text = "Username",
