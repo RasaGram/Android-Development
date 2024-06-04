4850fe606a1eba26d0983102aebf1b76690fde3b
Wery <44234831+Wery-wer@users.noreply.github.com>
2024-06-04 21:12:31 +0700

    update model

---
+++ app/src/main/java/com/dicoding/rasagram/data/model/User.kt
@@ -0,0 +1,9 @@
+package com.dicoding.rasagram.data.model
+
+data class User(
+    val id: Int,
+    val name: String,
+    val email : String,
+    val password : String,
+    val join_date : String,
+)
